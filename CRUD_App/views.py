4905979cb56bad4e36e27ec52f72d1475d25d9f5
andres-trrs <andres.torres32@inacapmail.cl>
2024-11-19 19:41:42 -0300

    eliminación lista

--- CRUD_App/views.py
+++ CRUD_App/views.py
@@ -18,4 +18,14 @@ def agregar_alumno(request):
 def eliminar_alumno(request):
     alumnos = Alumnos.objects.all()
     data = {'al': alumnos}
+    if request.method == 'POST':
+        alumno_id = request.POST.get('alumno_id')
+        if alumno_id:
+            # Elimina el alumno de la base de datos
+            try:
+                alumno = Alumnos.objects.get(id=alumno_id)
+                alumno.delete()
+            except Alumnos.DoesNotExist:
+                pass  # Si no existe el alumno, no hacemos nada (se podría agregar un mensaje de error)
+            return redirect('eliminar_alumno') 
     return render (request, 'eliminar_alumno.html', data)
