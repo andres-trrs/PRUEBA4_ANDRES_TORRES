654e471146c5b7c05aa0a4524240b479851dfae2
andres-trrs <andres.torres32@inacapmail.cl>
2024-12-12 23:00:32 -0300

    falta usuario normal y eliminar superuser

--- PRUEBA4_ANDRES_TORRES/urls.py
+++ PRUEBA4_ANDRES_TORRES/urls.py
@@ -7,4 +7,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('admin_page/', views.admin_page, name='admin_page'),
     path('user/', views.user_page, name='user_page'),
+    path('agregar_cliente/', views.agregar_cliente, name='agregar_cliente'),
+    path('clientes_listado/', views.admin_listado_clientes, name='admin_clientes'),
+    path('buscar/', views.admin_buscar, name='admin_buscar'),
 ]
--- prueba4App/views.py
+++ prueba4App/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
-from django.contrib.auth.models import User
+from .models import Cliente
+from django.contrib.auth.decorators import user_passes_test
 
 # Create your views here.
 
@@ -8,9 +9,7 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-
         user = authenticate(request, username=username, password=password)
-
         if user is not None:
             auth_login(request, user)
             if user.is_superuser:
@@ -22,12 +21,41 @@ def login_view(request):
     
     return render(request, 'login.html')
 
+
 def admin_page(request):
     if not request.user.is_authenticated or not request.user.is_superuser:
         return redirect('login')  
     return render(request, 'admin.html')
 
+
 def user_page(request):
     if not request.user.is_authenticated:
         return redirect('login') 
     return render(request, 'user.html')
+
+
+def agregar_cliente(request):
+    if request.method == 'POST':
+        nombre = request.POST['nombre']
+        telefono = request.POST['telefono']
+        fecha_ingreso = request.POST['fecha_ingreso']
+        Cliente.objects.create(nombre=nombre, telefono=telefono, fecha_ingreso=fecha_ingreso)
+        return redirect('admin_page')
+    return render(request, 'admin.html')
+
+
+def admin_listado_clientes(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return redirect('login') 
+    clientes = Cliente.objects.all()  
+    return render(request, 'admin_listado.html', {'clientes': clientes})
+
+
+def admin_buscar(request):
+    if not request.user.is_authenticated or not request.user.is_superuser:
+        return redirect('login') 
+    search = request.GET.get('search', '')
+    clientes = []
+    if search:
+        clientes = Cliente.objects.filter(nombre__icontains=search) | Cliente.objects.filter(id__icontains=search)
+    return render(request, 'admin_buscar.html', {'clientes': clientes, 'search': search})
